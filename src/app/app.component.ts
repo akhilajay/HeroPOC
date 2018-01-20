57811bccf92a0bad73dd9c5a4ba6a60ab78399aa
akhilajay <kannanmelathil@gmail.com>
2018-01-20 23:45:25 +0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero} from './hero';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  title = 'Tour of Heros';
+  heros=[new Hero(1,'DQ','http://img.nowrunning.com/content/Artist/DulquerSalman/banner.jpg'),
+  new Hero(2,'SRK','http://gdtmedia.india.com/data/topics/image/1/34471/c4995f9e9138ec4f766cf336190bd152_org_225X300_1.jpg'),
+  new Hero(3,'RAJANI',''),
+  new Hero(4,'LAL','')];
+  myHero=this.heros[0];
+  isUnchanged = false;
+
 }
